f97c5b6d58253e6ad9c379f930b0d9ecdc5443c3
Soham <sohambhambere5050@gmail.com>
2026-01-30 19:36:22 +0530

    refactor: Changed the CustomException class

--- src/exception.py
+++ src/exception.py
@@ -1,25 +1,36 @@
 import sys
+from types import TracebackType
+from typing import Optional
 
-def error_message_detail(error, error_detail: sys):
+
+def error_message_detail(error: Exception, error_detail):
     """
     Returns detailed error message with file name and line number
     """
-    _, _, exc_tb = error_detail.exc_info()
-    file_name = exc_tb.tb_frame.f_code.co_filename
-    line_number = exc_tb.tb_lineno
-    
-    error_message = f"Error occurred in script: [{file_name}] at line [{line_number}]: {str(error)}"
-    
-    return error_message
+    exc_type, exc_value, exc_tb = sys.exc_info()
+
+    if exc_tb is not None:
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        line_number = exc_tb.tb_lineno
+    else:
+        file_name = "Unknown file"
+        line_number = "Unknown line"
+
+    return (
+        f"Error occurred in script: [{file_name}] "
+        f"at line [{line_number}] "
+        f"error message: [{str(error)}]"
+    )
 
 
 class CustomException(Exception):
     """
     Custom exception class for better error tracking
     """
-    def __init__(self, error_message, error_detail: sys):
-        super().__init__(error_message)
-        self.error_message = error_message_detail(error_message, error_detail=error_detail)
-    
+
+    def __init__(self, error: Exception):
+        super().__init__(str(error))
+        self.error_message = error_message_detail(error, sys)
+
     def __str__(self):
         return self.error_message
